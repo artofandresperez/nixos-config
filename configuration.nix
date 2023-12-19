2f1f52d4efb74a97ec4e4c0758fb4a24c7a88fec
Andres Perez <artofandresperez@gmail.com>
2023-12-19 08:12:41 -0800

    this is messase

---
+++ configuration.nix
@@ -0,0 +1,245 @@
+ #this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+#af
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+
+
+  #boot 
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Los_Angeles";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+  
+  
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+ 
+  # AUDIO
+
+  #sound.enable = true;
+  #hardware.pulseaudio.enable = true;
+  #hardware.pulseaudio.support32Bit = true;
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  #enable flakes
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+
+  # setup NUR
+  nixpkgs.config.packageOverrides = pkgs: {
+    nur = import (builtins.fetchTarball {
+        url = "https://github.com/nix-community/NUR/archive/master.tar.gz";
+        sha256 = "0w5vvx765q746lyyy44xbyd1jqabk1aqfaq8pvf9h8k7f712yf87"   ;
+
+    }){
+    inherit pkgs;
+    };
+
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.amoeba = {
+    isNormalUser = true;
+    description = "amoeba";
+    extraGroups = [ "networkmanager" "wheel" "audio" "libvirtd" "tss"];
+    packages = with pkgs; [];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.nvidia.acceptLicense = true;
+
+  #set up virtualisation
+
+
+  # Enable dconf (System Management Tool)
+  programs.dconf.enable = true;
+
+  # Manage the virtualisation services
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      qemu = {
+        swtpm.enable = true;
+        ovmf.enable = true;
+        ovmf.packages = [ pkgs.OVMFFull.fd ];
+      };
+    };
+    spiceUSBRedirection.enable = true;
+  };
+  services.spice-vdagentd.enable = true;
+
+
+
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    lynx
+    dmenu
+    immersed-vr
+    telegram-desktop
+    lshw
+    pulsemixer
+    vifm
+    bottom 
+    nvtop
+    blender
+    parsec-bin
+    arandr
+    cudatoolkit
+    linuxPackages.nvidia_x11
+    libGLU
+    libGL
+    lutris
+    wine
+    winetricks
+    qemu
+    virt-manager
+    virt-viewer
+    spice spice-gtk
+    spice-protocol
+    win-virtio
+    win-spice
+    gnome.adwaita-icon-theme
+    virtiofsd
+    slack
+    zoom-us
+    brightnessctl
+    gh
+    gitkraken
+
+
+
+  ];
+  programs.git = {
+    enable = true;
+    lfs.enable = true;
+
+
+  };
+
+  programs.steam = {
+    enable = true;
+
+  };
+
+
+  services.xserver = {
+    enable = true;
+    exportConfiguration = true;
+
+    desktopManager = {
+      xterm.enable = false;
+    };
+    displayManager ={
+      startx.enable = true;
+
+    };
+    videoDrivers = ["nvidiaBeta"];
+    
+    wacom.enable = true;
+  };
+  hardware.opentabletdriver.enable = true;
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+    extraPackages = with  pkgs; [
+    	intel-media-driver
+	vaapiIntel
+	vaapiVdpau
+	libvdpau-va-gl
+
+    ];
+
+  };
+  hardware.nvidia = {
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    modesetting.enable = true;
+    open = false;
+    nvidiaSettings = true;
+    prime = {
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+      sync.enable = true;
+    };
+
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+}
---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "A very basic flake";
+
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+
+    };
+    nur.url = "github:nix-community/NUR";
+
+
+  };
+
+  outputs = { self, nixpkgs , home-manager, nur , ... }: 
+    let 
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+
+      };
+      lib = nixpkgs.lib;
+
+    in { 
+      nixosConfigurations = {
+        amoeba = lib.nixosSystem {
+          inherit system;
+          modules = [ ./configuration.nix 
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.amoeba = import ./home.nix;
+          }
+          nur.nixosModules.nur
+
+          ];
+        };
+
+      };
+    };
+
+
+
+  }
