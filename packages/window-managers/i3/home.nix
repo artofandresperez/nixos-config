b65fd7b8c1c7275068d62bf46a5cdacb1e494fca
Andres Perez <artofandresperez@gmail.com>
2024-01-24 08:10:59 -0800

    added polybar

--- packages/window-managers/i3/home.nix
+++ packages/window-managers/i3/home.nix
@@ -29,6 +29,37 @@ in
 
 
       };
+      bars = [];
+      
+
+    };
+    extraConfig = "
+      exec polybar \n
+
+    ";
+
+  };
+  services.polybar = {
+    enable = true;
+    script = "
+      polybar bar & \n
+
+    ";
+    config = {
+      "bar/top" = {
+        "bottom" = true;
+        font-0 = "JetBrainsMono-Regular:size=8;2";
+        modules-right = "date";
+      };
+      "module/date" = {
+        type = "internal/date";
+        internal = 5;
+        date = "%m/%d/%y";
+        time = "%H:%M";
+        label = "%time% %date%";
+
+
+      };
 
     };
 
